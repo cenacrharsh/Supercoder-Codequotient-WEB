cb07e14db2ccf70136fb0a82766f9e3feb598eee
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-03-25 12:11:37 +0530

    files added

--- week 8/authentication/client/script.js
+++ week 8/authentication/client/script.js
@@ -1,4 +1,5 @@
 const signInBtn = document.getElementById("sign-in-btn");
+const emailInput = document.getElementById('')
 
 //! adding click event listener to sign in button
 signInBtn.addEventListener("click", handleSignIn);
