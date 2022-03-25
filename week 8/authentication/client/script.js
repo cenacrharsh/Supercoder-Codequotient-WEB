e5ec2b8c7125618a43672794451257a2b20d21c2
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2022-03-25 12:50:12 +0530

    sign in route created

--- week 8/authentication/client/script.js
+++ week 8/authentication/client/script.js
@@ -1,9 +1,32 @@
 const signInBtn = document.getElementById("sign-in-btn");
-const emailInput = document.getElementById('')
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
 
 //! adding click event listener to sign in button
 signInBtn.addEventListener("click", handleSignIn);
 
-function handleSignIn() {
-  console.log("sign in");
+function handleSignIn(event) {
+  event.preventDefault();
+
+  let email = emailInput.value;
+  let password = passwordInput.value;
+  console.log(email, password);
+
+  let userDetails = {
+    email: email,
+    password: password,
+  };
+
+  sendFormDataToServer(userDetails, function () {});
+}
+
+function sendFormDataToServer(userDetails, callback) {
+  console.log(userDetails);
+  let request = new XMLHttpRequest();
+  request.open("POST", "/sign-in");
+  request.setRequestHeader("Content-Type", "application/json");
+  request.send(JSON.stringify(userDetails));
+  request.addEventListener("load", function () {
+    console.log("data sent to server");
+  });
 }
--- week 8/authentication/main.js
+++ week 8/authentication/main.js
@@ -1,12 +1,34 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
 
 const PORT = 3000;
 
 app.use(express.static("client"));
+app.use(express.json());
 
-app.post("/sign-in", function (req, res) {});
+function read(url, callback) {
+  fs.readFile(url, "utf-8", function (err, data) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(data);
+    }
+  });
+}
+
+app.post("/sign-in", function (req, res) {
+  let userDetails = req.body;
+  console.log(userDetails);
+  read("./db.txt", function (data) {
+    if (data.hasOwnProperty("errno")) {
+      res.end("Error in Reading Data from DB");
+    } else {
+      res.end(data);
+    }
+  });
+});
 
 app.post("/sign-up", function (req, res) {});
 
